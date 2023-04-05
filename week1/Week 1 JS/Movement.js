6bb8b8278e0ae6125787e7f8831638ef68cc6b4b
Dylan Shine <dyshiney11@gmail.com>
2023-04-05 15:25:43 -0400

    a

--- week1/Week 1 JS/Movement.js
+++ week1/Week 1 JS/Movement.js
@@ -1,11 +1,11 @@
 var canvas;
 var context;
 var timer;
-var numCollision;
+var numCollision = 0;
 var interval = 1000/60;
 var player;
-
-var speed = 0.1;
+var wall1;
+var speed = 0.10;
 
 //Set Up the Canvas
 canvas = document.getElementById("canvas");
@@ -13,6 +13,7 @@ context = canvas.getContext("2d");
 	
 //Instantiate the Player
 player = new Player();
+wall1 = new Wall();
 	
 //Set the Animation Timer
 timer = setInterval(animate, interval);
@@ -23,67 +24,89 @@ function animate()
 	//Erase the Screen
 	context.clearRect(0,0,canvas.width, canvas.height);	
 
-    function randomRange(high, low){
+    //collision stat
+    context.save();
+            context.fillStyle = "black";
+            context.font = "bold 16px Arial";
+			context.fillText("Collisions: " + numCollision, 600, 20, 100)
+            context.fillText("Acceleration: " + speed, 720, 20, 250)
+	context.restore();
+
+
+    function randomRange(high, low)
+    {
         return Math.random() * (high - low) + low;
+    }
+    function stats()
+    {
+        numCollision++;
+    }
+    function changeSize()
+    {
+        player.radius = randomRange(50,20)//25 default
     }
 
     function speedPlus()
     {
         player.vx += speed
         player.vy += speed
-        speed += 0.2
+        speed = speed + 0.10
         
-        if(speed >= 2)
+        if(speed >= 3.00)
         {
-            speed = 0.2;
+            speed = 0;
             player.vx = 10
             player.vy = 10
             //player.x = 0
             //player.y =0
         }
     }
-    function stats()
-    {
-        numCollision++;
-        context.save();
-			context.fillText("Collisions: " + numCollision, 600, 20, 50)
-		context.restore();
-    }
+   
 
     //bottom of canvas
     if (player.y > canvas.height - player.radius) {
         player.y = canvas.height - player.radius
         player.color = `rgb(${randomRange(255, 0)}, ${randomRange(255, 0)}, ${randomRange(255, 0)})`
         player.vy = -player.vy;
+        
         speedPlus()
         stats()
+        changeSize()
     }
     //top of the canvas
     if (player.y < 0 + player.radius) {
         player.y = player.radius;
         player.color = `rgb(${randomRange(255, 0)}, ${randomRange(255, 0)}, ${randomRange(255, 0)})`
-        player.vy = player.vy * randomRange(-1.3,-1);
+        player.vy = player.vy * randomRange(-1.2,-1);
+        
         speedPlus()
         stats()
+        changeSize()
     }
     //right side of the canvas
     if (player.x > canvas.width - player.radius) {
         player.x = canvas.width - player.radius
         player.color = `rgb(${randomRange(255, 0)}, ${randomRange(255, 0)}, ${randomRange(255, 0)})`
         player.vx = -player.vx;
+        
         speedPlus()
         stats()
+        changeSize()
     }
     //left side of the canvas
     if (player.x < player.radius) {
         player.x = player.radius
         player.color = `rgb(${randomRange(255, 0)}, ${randomRange(255, 0)}, ${randomRange(255, 0)})`
-        player.vx = player.vx * randomRange(-1.3,-1);
+        player.vx = player.vx * randomRange(-1.2,-1);
+        
         speedPlus()
         stats()
+        changeSize()
     }
     
     
+    wall1.Wall();
+
 	player.draw();
     player.move();
     
--- week1/Week 1 JS/Player.js
+++ week1/Week 1 JS/Player.js
@@ -38,3 +38,26 @@ function Player()
         this.y += this.vy;
     }
 }
+
+function Wall(x1, y1, w1, h1)
+{
+	this.x = x1
+	this.y = y1
+	
+	//wall demensions
+	this.width = w1;
+	this.height = h1;
+
+
+	this.color = "#000000";
+
+	this.createWall = function()
+	{
+		context.save();
+            context.fillStyle = this.color;
+            context.fillRect(this.x, this.y, this.width, this.height);
+            context.fill();
+		context.restore();
+	}	
+
+}
