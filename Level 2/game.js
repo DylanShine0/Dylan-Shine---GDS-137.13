60b53f6992e53b762ff79804e6b9936d6c8a90e5
Dylan Shine <dyshiney11@gmail.com>
2023-04-06 15:50:25 -0400

    a

--- Level 2/game.js
+++ Level 2/game.js
@@ -3,8 +3,13 @@ var context;
 var timer;
 var interval = 1000/60;
 
+
+
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");	
+
+var player1 = new GameObject(10, 400, 10, 100, "red");
+
 
 
 timer = setInterval(animate, interval);
@@ -14,5 +19,5 @@ function animate()
     context.clearRect(0,0,canvas.width, canvas.height);	
 
 
-    
+    player1.drawRect();
 }
