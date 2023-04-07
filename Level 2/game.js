29bedc27d6986bc44fa6fc9d320f55733bf40286
Dylan Shine <dyshiney11@gmail.com>
2023-04-07 12:06:35 -0400

    a

--- Level 2/game.js
+++ Level 2/game.js
@@ -10,7 +10,10 @@ context = canvas.getContext("2d");
 
 var player1 = new GameObject(10, 400, 10, 100, "red");
 
-
+lBlock1 = new GameObject(canvas.width - 750, canvas.height/2+75, 100, 100,"#00ff00");
+lBlock2 = new GameObject(canvas.width - 550, canvas.height/2+75, 100, 100,"#00ff00");
+rBlock1 = new GameObject((canvas.width-350), canvas.height/2, 100, 100, "orange");
+rBlock2 = new GameObject((canvas.width-50), canvas.height/2, 100, 100, "blue");
 
 timer = setInterval(animate, interval);
 
@@ -18,6 +21,34 @@ function animate()
 {
     context.clearRect(0,0,canvas.width, canvas.height);	
 
+    if(w)
+    {
+        console.log("Moving Up");
+        player1.y += -2;
+    }
+    if(s)
+    {
+        console.log("Moving Down");
+        player1.y += 2;
+    }
+    /*
+	if(a)
+	{
+		console.log("Moving Left");
+		player.x += -2;
+	}
+    if(d)
+	{
+		console.log("Moving Right");
+		player.x += 2;
+	}
+    */
 
     player1.drawRect();
+
+    //Test Elements
+    lBlock1.drawCircle();
+	lBlock2.drawCircle();
+	rBlock1.drawRect();
+	rBlock2.drawRect();
 }
