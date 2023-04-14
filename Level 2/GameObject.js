7c2e5dde3ee9bfb6548e09ec9f3c3295a46bbd38
Dylan Shine <dyshiney11@gmail.com>
2023-04-14 12:25:13 -0400

    a

--- Level 2/GameObject.js
+++ Level 2/GameObject.js
@@ -35,7 +35,6 @@ function GameObject(x,y,w,h,color)
 			context.translate(this.x, this.y);
 			context.beginPath();
 			context.arc(0, 0, this.width/2, 0, 360 *Math.PI/180, true);
-			
 			context.closePath();
 			context.fill();
 		context.restore();
