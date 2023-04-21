a88d01ddabbedd02d8795748f31e2350e4a451e5
Dylan Shine <dyshiney11@gmail.com>
2023-04-21 14:55:27 -0400

    a

--- Level 3 Prototype Mechanic/game.js
+++ Level 3 Prototype Mechanic/game.js
@@ -5,13 +5,15 @@ var interval = 1000/60;
 
 var score1 = 0;
 
+var frictionX = 0.4;	
+//var frictionY = .97;
 
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");	
 
 //var img = document.getElementById("ric"); //going to be used later for ship
 
-var Player1 = new GameObject(canvas.width/2, canvas.height/2, 50, 50, "red");
+var Player1 = new GameObject(200, 200, 50, 50, "red");
 
 Player1.vx = 0;
 Player1.vy = 0;
@@ -34,41 +36,53 @@ function animate()
 
 
     //Player1 movement 
-    if(w)//up
-    {
-        console.log("Moving Up");
-        Player1.y += -4;
-    }  
-    if(s)//down
-    {
-        console.log("Moving Down");
-        Player1.y += 4;
-    } 
-
-    if(d)
-    {
-        Player1.vx = 5;
-    }
-    if(a)
-    {
-        Player1.vx = -5;
-    }
+    if(w){Player1.vx = 0; Player1.vy = -5;}//up  
+    if(s){Player1.vx = 0; Player1.vy = 5;}//down
+    if(d){Player1.vx = 5; Player1.vy = 0;}//right
+    if(a){Player1.vx = -5; Player1.vy = 0;}//left
 
     
 
+    //up  && right
+    if(w && d)
+    {
+        Player1.vy = -5;
+        Player1.vx = 5;
+    }
+    //up && left
+    if(w && a)
+    {
+        Player1.vy = -5;
+        Player1.vx = -5;
+    }
+    //down && left
+    if(s && a)
+    {
+        Player1.vy = 5;
+        Player1.vx = -5;
+    }
+    //down && right
+    if(s && d)
+    {
+        Player1.vy = 5;
+        Player1.vx = 5;
+    }
+    
+    
+    
 
 
 
     //Player1 COLLISION      screen boundary
     //top of the canvas
-    if (Player1.y < 0 + Player1.height/2) 
+    if (Player1.y < 0 + Player1.width/2) 
     {
-        Player1.y = Player1.height/2;
+        Player1.y = 0 + Player1.width/2;
     }
     //bottom of canvas
-    if (Player1.y > canvas.height - Player1.height/2) 
+    if (Player1.y > canvas.height - Player1.width/2) 
     {
-        Player1.y = canvas.height - Player1.height/2
+        Player1.y = canvas.height - Player1.width/2
     }
     //left of canvas
     if(Player1.x < 0 + Player1.width/2)
@@ -76,7 +90,7 @@ function animate()
         Player1.x = 0 + Player1.width/2;
     }
     //right of canvas
-    if(Player1.x < canvas.width - Player1.width/2)
+    if(Player1.x > canvas.width - Player1.width/2)
     {
         Player1.x = canvas.width - Player1.width/2;
     }
@@ -85,5 +99,31 @@ function animate()
 
     Player1.move();
     Player1.drawCircle();
+    //showFriction();
 
+}
+
+function showFriction()
+{
+	if(d)
+	{	
+		Player1.vx += Player1.ax * Player1.force;
+	}
+	if(a)
+	{
+		Player1.vx += Player1.ax * -Player1.force;
+	}
+	if(w)
+	{	
+		Player1.vy += Player1.ay * -Player1.force;
+	}
+	if(s)
+	{
+		Player1.vy += Player1.ay * Player1.force;
+	}
+	
+	//--------------Apply friction to the Velocity X-----------------------------------------
+	//Player1.vx *= frictionX;
+	//---------------------------------------------------------------------------------------
+	//Player1.x += Player1.vx;
 }
