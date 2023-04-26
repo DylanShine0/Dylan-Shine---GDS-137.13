f10a85b6df559ac90e3525547592702988d43581
Dylan Shine <dyshiney11@gmail.com>
2023-04-26 15:12:21 -0400

    a

---
+++ Level 3 Paddle Boss/controls.js
@@ -0,0 +1,33 @@
+var a = false; //65
+var d = false; //68
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+function press(e)
+{
+	
+	//console.log("Pressed" + e.keyCode);
+
+	if(e.keyCode == 65){
+        a = true;
+    }
+	if(e.keyCode == 68){
+        d = true;
+    }
+}
+
+function release(e)
+{
+	
+	//console.log("Released" + e.keyCode);
+	
+	if(e.keyCode == 65)
+    {
+        a = false;
+    }
+	if(e.keyCode == 68)
+    {
+        d = false;
+    }
+}
